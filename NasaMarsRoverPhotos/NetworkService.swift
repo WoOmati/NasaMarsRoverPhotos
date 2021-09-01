9b8892a1e1143f680abb5d2db116ca11502eaf33
Rasul <>
2021-09-02 00:11:27 +0300

    вернул нетворк сервис

---
+++ NasaMarsRoverPhotos/NetworkService.swift
@@ -0,0 +1,24 @@
+//
+//  NetworkService.swift
+//  NasaMarsRoverPhotos
+//
+//  Created by macbook on 01.09.2021.
+//
+
+import Foundation
+
+class NetworkService {
+
+    func request(url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                guard let data = data else { return }
+                completion(.success(data))
+            }
+        }.resume()
+    }
+}
